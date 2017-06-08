17a3583c69641aead124207a821dc747cedf3e3b
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-06-08 13:28:51 -1000

    darth vadar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("sanity check");
+function requestHelper(link, listener) {
+  var newReq = new XMLHttpRequest();
+  newReq.addEventListener("load", listener);
+  newReq.open("GET", link);
+  newReq.send();
+}
+
+
+function personFour() {
+  var personObject = JSON.parse(this.responseText);
+  document.getElementById("person4Name").innerHTML = personObject.name;
+
+}
+
+requestHelper("http://swapi.co/api/people/4/", personFour);
