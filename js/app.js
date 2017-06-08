bac6d06cfd91d9cc4b9a11582d2344662d8828b8
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-06-08 13:31:31 -1000

    homeworld for Mr. Vadar

--- js/app.js
+++ js/app.js
@@ -10,6 +10,13 @@ function personFour() {
   var personObject = JSON.parse(this.responseText);
   document.getElementById("person4Name").innerHTML = personObject.name;
 
+  function homeRequest() {
+  var homeWorldObject = JSON.parse(this.responseText);
+  document.getElementById("person4HomeWorld").innerHTML = homeWorldObject.name;
+  }
+  requestHelper(personObject.homeworld, homeRequest);
 }
+
+
 
 requestHelper("http://swapi.co/api/people/4/", personFour);
